760e22aeb504a0d9ff628c4d3b6fb87603b94ec9
sashok <wcg15@mail.ru>
2017-04-24 23:32:32 +0300

    Added models, delete comments

--- go/main.go
+++ go/main.go
@@ -79,31 +79,4 @@ func main() {
 	//// Update - update product's price to 2000
 	//db.Model(&product).Update("Price", 2000)
 }
-//var tpl *template.Template
-//
-//func init()  {
-//	tpl = template.Must(template.ParseGlob("templates/*"))
-//
-//}
-//
-//type person struct {
-//	FirstName string
-//	LastName string
-//}
-//
-//func main()  {
-//	http.HandleFunc("/", foo)
-//	http.ListenAndServe(":8080", nil)
-//}
-//
-//
-//
-//func foo(w http.ResponseWriter, req *http.Request)  {
-//	f := req.FormValue("first")
-//	l := req.FormValue("last")
-//	err := tpl.ExecuteTemplate(w, "index.html", person{f, l})
-//	if err != nil{
-//		http.Error(w, err.Error(), 500)
-//		log.Fatalln(err)
-//	}
-//}
+
